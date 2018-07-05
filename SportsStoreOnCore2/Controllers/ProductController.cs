f704ff10a681d27234ccdbc7750894f0a69c6dfe
Denis Pidavashetskyi <pod.den1804@gmail.com>
2018-07-05 18:04:21 +0300

    Создание контроллера + вюшек + создание репозитория + прописывание маршрута

---
+++ SportsStoreOnCore2/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStoreOnCore2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreOnCore2.Controllers
+{
+    public class ProductController : Controller 
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
