a9e9371c4ce4e3947e103c8aecdf0f295052e3b1
Denis Pidavashetskyi <pod.den1804@gmail.com>
2018-07-05 16:50:27 +0300

    Creating folders and _ViewImports

--- SportsStoreOnCore2/Startup.cs
+++ SportsStoreOnCore2/Startup.cs
@@ -15,6 +15,7 @@ namespace SportsStoreOnCore2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,16 @@ namespace SportsStoreOnCore2
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+
             });
         }
     }
